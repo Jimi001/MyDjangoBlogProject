44210abb837bbbe0e9573fe9e3a59762c7a5137b
Folajimi <jimi@example.com>
2024-05-04 08:47:51 +0100

    create blog

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts': Post.objects.all(),
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You are now able to login!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
