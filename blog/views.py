9005cc4b67f4cd25dcfeeacbb7fc0ac56ce25d64
Folajimi <jimi@example.com>
2024-05-09 12:19:45 +0100

    update user profile

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -8,6 +8,16 @@ def home(request):
         'posts': Post.objects.all(),
     }
     return render(request, 'home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
 
 def about(request):
     return render(request, 'about.html', {'title': 'About'})
