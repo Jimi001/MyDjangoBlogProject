322aa3512b0fe9ebb056b4d5af85627cf0756c36
Folajimi <jimi@example.com>
2024-05-13 14:47:28 +0100

    implement class based view CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,21 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView, 
+    UpdateView, 
+    DeleteView,
+)
 from .models import Post
 
-# Create your views here.
-def home(request):
-    context = {
-        'posts': Post.objects.all(),
-    }
-    return render(request, 'home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all(),
+#     }
+#     return render(request, 'home.html', context)
 
 class PostListView(ListView):
     model = Post
@@ -17,7 +25,42 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+    template_name = 'post_detail.html'
     
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_form.html'
+    fields = ['title', 'content', ]
 
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'post_form.html'
+    fields = ['title', 'content', ]
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
 def about(request):
     return render(request, 'about.html', {'title': 'About'})
