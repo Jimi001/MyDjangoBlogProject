0d55af73c933952955347ec50ae838faabfbcd7c
Folajimi <jimi@example.com>
2024-05-16 18:00:39 +0100

    email and password reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,3 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
